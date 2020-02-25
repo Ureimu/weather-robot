942e03074a48116cf4625b5cef9c960e3ff31067
Ureimu <a1090693441@163.com>
2020-02-25 15:52:11 +0800

    create weather plugin

---
+++ python_code/config.py
@@ -0,0 +1,6 @@
+from nonebot.default_config import *
+
+SUPERUSERS = {1090693441}
+COMMAND_START = {'', '/', '!', '／', '！'}
+HOST = '0.0.0.0'
+PORT = 8080
--- python_code/main.py
+++ python_code/main.py
@@ -1,6 +1,10 @@
 import nonebot
+import config
+from os import path
 
 if __name__ == '__main__':
-    nonebot.init()
-    nonebot.load_builtin_plugins()
-    nonebot.run(host='127.0.0.1', port=8080)
+    nonebot.init(config)
+    nonebot.load_plugins(path.join(path.dirname(__file__), 'botcode', 'plugins'),
+    'botcode.plugins'
+    )
+    nonebot.run()
